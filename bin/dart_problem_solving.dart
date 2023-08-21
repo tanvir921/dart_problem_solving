9ba23ce8874ba31032db8391ad65622dba44f21e
tanvir21 <tanvirahmedbinaahali@gmail.com>
2023-08-21 20:35:54 +0600

    first commit

---
+++ bin/dart_problem_solving.dart
@@ -0,0 +1,6 @@
+import 'package:dart_problem_solving/dart_problem_solving.dart'
+    as dart_problem_solving;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_problem_solving.calculate()}!');
+}
---
+++ bin/user_input_and_print.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("What's your name? ");
+  String name = stdin.readLineSync().toString();
+
+  print("Hi, $name! What is your age?");
+  int age = int.parse(stdin.readLineSync().toString());
+
+  int yearsToHunderd = 100 - age;
+  print("$name, You have $yearsToHunderd years to be 100");
+}
